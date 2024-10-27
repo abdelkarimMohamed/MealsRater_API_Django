3c2b041d341f193c5f7a889de6c5b8bb57f8e879
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-10-27 03:07:14 +0300

    add api meal create && update_rate meal && meal list api avg and numbers of rating

--- api/views.py
+++ api/views.py
@@ -1,10 +1,50 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from .models import Meal, Rating
 from .serializers import MealSerializer, RatingSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+
 
 class MealViewSet(viewsets.ModelViewSet):
     queryset=Meal.objects.all()
     serializer_class = MealSerializer
+
+    @action(detail=True, methods=['post'])
+    def rate_meal(self,request,pk=None):
+        
+        if 'stars' in request.data:
+
+            meal = Meal.objects.get(id=pk)
+            stars = request.data['stars']
+            username = request.data['username']
+            user = User.objects.get(username=username)
+
+            try:
+            
+                rating=Rating.objects.get(user=user.id,meal=meal.id)
+                rating.stars = stars
+                rating.save()
+                serializer=RatingSerializer(rating,many=False)
+                json = {
+                    'message': 'Meal Rate Updated',
+                    'result': serializer.data
+                }
+                return Response(json , status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(stars=stars, meal=meal, user=user)
+                serializer = RatingSerializer(rating, many=False)
+                json = {
+                    'message': 'Meal Rate Created',
+                    'result': serializer.data
+                }
+                return Response(json , status=status.HTTP_201_CREATED)
+        else:
+            json = {
+                'message': 'stars not provided'
+            }
+            return Response(json , status=status.HTTP_400_BAD_REQUEST)
+
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset=Rating.objects.all()
